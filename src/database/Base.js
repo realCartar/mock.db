c5ea9d005000075551d1ebbdc58cebb9cdecbc76
realCartar <39052179+realCartar@users.noreply.github.com>
2020-08-03 19:52:35 +1000

    add the table thingy

--- src/database/Base.js
+++ src/database/Base.js
@@ -2,7 +2,17 @@ const EventEmitter = require('events');
 const fs = require('fs');
 
 class table extends EventEmitter {
-
+  constructor() {
+    super();
+    this.tableName = name;
+    this.events = new EventEmitter();
+    this.separator = '/'; if(process.platform === 'win32') this.separator = '\\';
+    if(!fs.existsSync(process.cwd() + `${this.separator}database`)) fs.mkdir(process.cwd() + `${this.separator}database${this.separator}`);
+    if(!fs.existsSync(process.cwd() + `${this.separator}database${this.separator}${name}`)) {
+      const data = fs.mkdirSync(process.cwd() + `${this.separator}database${this.separator}${name}`);
+      this.events.emit('tableCreated', data);
+    }
+  }
 }
 
 module.exports = table;
