bf55517d0af12cfbfbdaf5a930522b7db5d3ba53
realCartar <39052179+realCartar@users.noreply.github.com>
2020-08-03 19:57:40 +1000

    idek anymore
    ig i added all of the planned methods that i wanted

--- src/database/Extended.js
+++ src/database/Extended.js
@@ -7,6 +7,17 @@ class funkytown extends table {
     const data = fs.readFileSync(`${process.cwd()}${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`);
     return JSON.parse(data).val;
   }
+  set(entry = String, data = Any) {
+    let path = `${process.cwd()}${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`;
+    if(!fs.existsSync(path)) return fs.appendFileSync(path, JSON.stringify({val: data}));
+    return data;
+  }
+  delete(entry = String) {
+    let path = `${process.cwd()}${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`;
+    if(!fs.existsSync(path)) return undefined;
+    fs.unlinkSync(path);
+    return null;
+  }
 }
 
 module.exports = funkytown; // xd pls give me money bruv, i spent 5 minutes on this ez pzy lessss gooooooooo
