46e3c228872954bbbb43437fa0ab40476d454793
realCartar <39052179+realCartar@users.noreply.github.com>
2020-08-29 20:13:00 +1000

    i didnt test any of these updates.. lets hope it works..?

--- src/database/Extended.js
+++ src/database/Extended.js
@@ -1,24 +1,25 @@
-const fs = require('fs');
-const table = require('./Base');
+const fs = require('fs'),
+      path = require('path'),
+      table = require('./Base');
 
 class funkytown extends table {
   fetch(entry = String) {
     if(!entry) return console.error(new TypeError('no entry provided when fetching from a table'))
-    if(!fs.existsSync(process.cwd() + `${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`)) return null;
-    const data = fs.readFileSync(`${process.cwd()}${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`);
+    if(!fs.existsSync(path.join(process.cwd(), "database", this.tableName, `${entry}.json`))) return null;
+    const data = fs.readFileSync(path.join(process.cwd(), "database", this.tableName, `${entry}.json`));
     return JSON.parse(data).val;
   }
   set(entry = String, data = Any) {
     if(!entry) return console.error(new TypeError('no entry provided when setting to a table'));
     if(!data) return console.error(new TypeError('no data to set to an entry'));
-    let path = `${process.cwd()}${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`;
+    const path = path.join(process.cwd(), "database", this.tableName, `${entry}.json`);
     if(!fs.existsSync(path)) return fs.appendFileSync(path, JSON.stringify({val: data}));
     else fs.writeFileSync(path, JSON.stringify({val: data}));
     return data;
   }
   delete(entry = String) {
-    if(!entry) return console.error(new TypeError('no entry provided when deleting from a table'))
-    let path = `${process.cwd()}${this.separator}database${this.separator}${this.tableName}${this.separator}${entry}.json`;
+    if(!entry) return console.error(new TypeError('no entry provided when deleting from a table'));
+    let path = path.join(process.cwd(), "database", this.tableName, `${entry}.json`);
     if(!fs.existsSync(path)) return null;
     fs.unlinkSync(path);
     return null;
