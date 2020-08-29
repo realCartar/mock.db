a80e2258049f5434cf50054e8a68436331563b45
realCartar <39052179+realCartar@users.noreply.github.com>
2020-08-29 20:08:46 +1000

    Update Base.js

--- src/database/Base.js
+++ src/database/Base.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
+const fs = require('fs'),
+      path = require('path'); // i dumb, i forgot path existed - ooga booga caveman brain.
 
 class table {
   constructor(name) {
     if(!name) return console.error(new TypeError('no table name provided'));
     this.tableName = name;
-    this.separator = '/'; if(process.platform === 'win32') this.separator = '\\';
-    if(!fs.existsSync(process.cwd() + `${this.separator}database`)) fs.mkdirSync(process.cwd() + `${this.separator}database${this.separator}`);
-    if(!fs.existsSync(process.cwd() + `${this.separator}database${this.separator}${name}`)) return fs.mkdirSync(process.cwd() + `${this.separator}database${this.separator}${name}`)
+    if(!fs.existsSync(path.join(process.cwd(), "database")) fs.mkdirSync(path.join(process.cwd(), "database"));
+    if(!fs.existsSync(path.join(process.cwd(), "database", name)) return fs.mkdirSync(path.join(process.cwd(), "database", name))
   }
 }
 
