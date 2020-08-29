ecf0be6be3b8cf06740a830295416380e864d38f
realCartar <39052179+realCartar@users.noreply.github.com>
2020-08-29 20:18:54 +1000

    Update Base.js

--- src/database/Base.js
+++ src/database/Base.js
@@ -5,8 +5,8 @@ class table {
   constructor(name) {
     if(!name) return console.error(new TypeError('no table name provided'));
     this.tableName = name;
-    if(!fs.existsSync(path.join(process.cwd(), "database")) fs.mkdirSync(path.join(process.cwd(), "database"));
-    if(!fs.existsSync(path.join(process.cwd(), "database", name)) return fs.mkdirSync(path.join(process.cwd(), "database", name))
+    if(!fs.existsSync(path.join(process.cwd(), "database"))) fs.mkdirSync(path.join(process.cwd(), "database"));
+    if(!fs.existsSync(path.join(process.cwd(), "database", name))) return fs.mkdirSync(path.join(process.cwd(), "database", name))
   }
 }
 
